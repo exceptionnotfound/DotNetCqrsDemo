7877a4b4f3f5213c9edd5dca3b780689cdcff04c
exceptionnotfound <mpjones428@gmail.com>
2016-08-17 07:46:34 -0700

    Added in memory event store and serialized event

---
+++ CQRSLiteDemo.Domain/EventStore/InMemoryEventStore.cs
@@ -0,0 +1,43 @@
+﻿using CQRSlite.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRSLiteDemo.Domain.EventStore
+{
+    public class InMemoryEventStore : IEventStore
+    {
+        private readonly IEventPublisher _publisher; //Use this to publish events so that event handlers can consume them
+        private readonly Dictionary<Guid, List<IEvent>> _inMemoryDb = new Dictionary<Guid, List<IEvent>>();
+
+        public InMemoryEventStore(IEventPublisher publisher)
+        {
+            _publisher = publisher;
+        }
+
+        public void Save<T>(IEnumerable<IEvent> events)
+        {
+            foreach (var @event in events)
+            {
+                List<IEvent> list;
+                _inMemoryDb.TryGetValue(@event.Id, out list);
+                if (list == null)
+                {
+                    list = new List<IEvent>();
+                    _inMemoryDb.Add(@event.Id, list);
+                }
+                list.Add(@event);
+                _publisher.Publish(@event);
+            }
+        }
+
+        public IEnumerable<IEvent> Get<T>(Guid aggregateId, int fromVersion)
+        {
+            List<IEvent> events;
+            _inMemoryDb.TryGetValue(aggregateId, out events);
+            return events?.Where(x => x.Version > fromVersion) ?? new List<IEvent>();
+        }
+    }
+}
---
+++ CQRSLiteDemo.Domain/EventStore/SerializedEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRSLiteDemo.Domain.EventStore
+{
+    public class SerializedEvent
+    {
+        public Guid EventId { get; set; }
+        public Guid AggregateId { get; set; }
+        public string Type { get; set; }
+        public string Event { get; set; }
+        public DateTime Timestamp { get; set; }
+        public int Version { get; set; }
+    }
+}
