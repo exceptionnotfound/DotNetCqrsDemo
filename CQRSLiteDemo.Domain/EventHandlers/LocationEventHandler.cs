1a088cdfe047a6cc2aac349bd09236b46c119e4c
exceptionnotfound <mpjones428@gmail.com>
2016-08-22 10:23:40 -0700

    Annotated the Event Handlers, working on DTO objects.

--- CQRSLiteDemo.Domain/EventHandlers/LocationEventHandler.cs
+++ CQRSLiteDemo.Domain/EventHandlers/LocationEventHandler.cs
@@ -13,7 +13,8 @@ using System.Threading.Tasks;
 namespace CQRSLiteDemo.Domain.EventHandlers
 {
     public class LocationEventHandler : IEventHandler<LocationCreatedEvent>,
-                                        IEventHandler<EmployeeAssignedToLocationEvent>
+                                        IEventHandler<EmployeeAssignedToLocationEvent>,
+                                        IEventHandler<EmployeeRemovedFromLocationEvent>
     {
         private readonly IConnectionMultiplexer _redis;
         private readonly IMapper _mapper;
@@ -23,23 +24,55 @@ namespace CQRSLiteDemo.Domain.EventHandlers
             _mapper = mapper;
         }
 
-        public void Handle(LocationCreatedEvent message)
-        {
-            var database = _redis.GetDatabase();
-            LocationDTO location = _mapper.Map<LocationDTO>(message);
-            database.StringSet("location:" + message.LocationID, JsonConvert.SerializeObject(location));
-        }
+    public void Handle(LocationCreatedEvent message)
+    {
+        //Get the Redis instance we will be interfacing with
+        var database = _redis.GetDatabase();
+
+        //Create a new LocationDTO object from the LocationCreatedEvent
+        LocationDTO location = _mapper.Map<LocationDTO>(message);
+
+        //Serialize the new LocationDTO into the Redis store, with key "location:{LocationID}"
+        database.StringSet("location:" + message.LocationID, JsonConvert.SerializeObject(location));
+    }
 
         public void Handle(EmployeeAssignedToLocationEvent message)
         {
+            //Get the Redis instance we will be interfacing with
             var database = _redis.GetDatabase();
+
+            //Deserialize the LocationDTO object specified by the "location:{locationID}" key
             var location = JsonConvert.DeserializeObject<LocationDTO>(database.StringGet("location:" + message.NewLocationID.ToString()));
+
+            //Add the specified employee to the LocationDTO object
             location.Employees.Add(message.EmployeeID);
+
+            //Re-serialize the changed LocationDTO
             database.StringSet("location:" + message.NewLocationID, JsonConvert.SerializeObject(location));
 
+            //Find the employee which was assigned to this Location
             var employee = JsonConvert.DeserializeObject<EmployeeDTO>(database.StringGet("employee:" + message.EmployeeID.ToString()));
+
+            //Set the employee's LocationID property
             employee.LocationID = message.NewLocationID;
+
+            //Re-serialize the employee back into the datastore.
             database.StringSet("employee:" + message.EmployeeID, JsonConvert.SerializeObject(employee));
+        }
+
+        public void Handle(EmployeeRemovedFromLocationEvent message)
+        {
+            //Get the Redis instance
+            var database = _redis.GetDatabase();
+
+            //Deserialize the LocationDTO object with the key "location:{locationID}"
+            var location = JsonConvert.DeserializeObject<LocationDTO>(database.StringGet("location:" + message.OldLocationID.ToString()));
+
+            //Remove the employee from the location
+            location.Employees.Remove(message.EmployeeID);
+
+            //Serialize the changed LocationDTO back into the data store.
+            database.StringSet("location:" + message.OldLocationID, JsonConvert.SerializeObject(location));
         }
     }
 }
